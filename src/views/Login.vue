d930086fb4cfc5f1e0697396ac54809e17b59a2f
liang999999 <a20060326@yahoo.com.tw>
2023-05-17 22:18:46 +0800

    liang02

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,7 +24,7 @@ export default {
         <h1>Log in Page / 元件範例</h1>
         <h2>元件說明: (元件內部也有註解說明)</h2>
         <p>請 props "searchCondition" 傳入搜索條件的陣列，並 @emmit 一個 SearchResponse ，會回傳 [搜索條件, 搜索關鍵字] 的陣列資訊</p>
-        <SearchBar :searchCondition="['物件','借主', '契約コード']"/>
+        <SearchBar :searchCondition="['物件','龍主', '契約コード','神主']"/>
 
         <p>請由外部綁定 v-if 作為顯示切換，並 props 傳入標題字串，@close綁定視窗右上角取消事件</p>
         <p></p>
