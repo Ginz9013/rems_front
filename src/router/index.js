5bd8edb3d7dee17d69eb2470075f17b109c12940
Kouhei <alan829221@gmail.com>
2023-05-23 22:48:19 +0800

    UPDATE: router

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,69 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      // 登入頁面
       path: '/',
       name: 'login',
       component: LoginView
-    }
+    },
+    {
+      // 首頁
+      path: '/home',
+      name: 'home',
+      // component:
+    },
+    {
+      // 物件首頁
+      path: '/bukken',
+      name: 'bukken',
+      // component: ,
+      children: [
+        {
+          // 物件詳細頁面(含修改)
+          path: 'info',
+          // component:
+        }
+      ]
+    },
+    {
+      // 契約首頁
+      path: '/Keiyaku',
+      name: 'Keiyaku',
+      // component:,
+      children: [
+        {
+          // 契約詳細頁面(包含歷史契約)
+          path: 'info',
+          // component:,
+        }
+      ]
+    },
+    {
+      // 貸主首頁
+      path: '/kashinushi',
+      name: 'kashinushi',
+      // component: ,
+      children: [
+        {
+          // 貸主詳細頁面(含修改)
+          path: 'info',
+          // component:
+        }
+      ]
+    },
+    {
+      // 借主首頁
+      path: '/karinushi',
+      name: 'karinushi',
+      // component: ,
+      children: [
+        {
+          // 借主詳細頁面(含修改)
+          path: 'info',
+          // component:
+        }
+      ]
+    },
   ]
 })
 
