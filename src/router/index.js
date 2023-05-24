15c86cf3ff49c7873a44b569e41a9d88a7e3b3db
Kouhei <alan829221@gmail.com>
2023-05-24 20:26:07 +0800

    FIX: router

--- src/router/index.js
+++ src/router/index.js
@@ -21,53 +21,49 @@ const router = createRouter({
       path: '/bukken',
       name: 'bukken',
       // component: ,
-      children: [
-        {
-          // 物件詳細頁面(含修改)
-          path: 'info',
-          // component:
-        }
-      ]
+    },
+    {
+      // 物件詳細頁面(含修改)
+      path: '/bukken/info',
+      name: 'bukkenInfo'
+      // component:
     },
     {
       // 契約首頁
-      path: '/Keiyaku',
-      name: 'Keiyaku',
+      path: '/keiyaku',
+      name: 'keiyaku',
       // component:,
-      children: [
-        {
-          // 契約詳細頁面(包含歷史契約)
-          path: 'info',
-          // component:,
-        }
-      ]
+    },
+    {
+      // 契約詳細頁面(包含歷史契約)
+      path: '/keiyaku/info',
+      name: 'keiyakuInfo',
+      // component:,
     },
     {
       // 貸主首頁
       path: '/kashinushi',
       name: 'kashinushi',
       // component: ,
-      children: [
-        {
-          // 貸主詳細頁面(含修改)
-          path: 'info',
-          // component:
-        }
-      ]
+    },
+    {
+      // 貸主詳細頁面(含修改)
+      path: '/kashinushi/info',
+      name: 'kashinushiInfo',
+      // component:
     },
     {
       // 借主首頁
       path: '/karinushi',
       name: 'karinushi',
-      // component: ,
-      children: [
-        {
-          // 借主詳細頁面(含修改)
-          path: 'info',
-          // component:
-        }
-      ]
+      // component: 
     },
+    {
+      // 借主詳細頁面(含修改)
+      path: '/karinushi/info',
+      name: 'karinushiInfo',
+      // component:
+    }
   ]
 })
 
