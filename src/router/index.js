9963fce3b2b249164911043b3a7e0305a2825273
TONYFU\WIN10 <tonyfu13@gmail.com>
2023-05-25 00:49:06 +0800

    TonyNew

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,71 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from "../views/Login.vue";
 import Homepage from "../views/Homepage.vue";
-import bukken from "../components/Property.vue";
-import kashinushi from "../components/addPropertyPage.vue"
+import Property from "../components/Property.vue";
+import addPropperty from "../components/addPropertyPage.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      // 登入頁面
       path: '/',
       name: 'login',
       component: LoginView
     },
     {
-      path: '/homepage',
-      name: 'homepage',
-      component: Homepage
+      // 首頁
+      path: '/home',
+      name: 'home',
+      // component:
     },
     {
+      // 物件首頁
       path: '/bukken',
-      name: 'Bukken',
-      component: bukken
+      name: 'bukken',
+      // component: ,
     },
     {
+      // 物件詳細頁面(含修改)
+      path: '/bukken/info',
+      name: 'bukkenInfo',
+      component:Property,
+    },
+    {
+      // 契約首頁
+      path: '/keiyaku',
+      name: 'keiyaku',
+      // component:,
+    },
+    {
+      // 契約詳細頁面(包含歷史契約)
+      path: '/keiyaku/info',
+      name: 'keiyakuInfo',
+      // component:,
+    },
+    {
+      // 貸主首頁
       path: '/kashinushi',
-      name: 'Kashinushi',
-      component: kashinushi
+      name: 'kashinushi',
+      // component: ,
+    },
+    {
+      // 貸主詳細頁面(含修改)
+      path: '/kashinushi/info',
+      name: 'kashinushiInfo',
+      // component:
+    },
+    {
+      // 借主首頁
+      path: '/karinushi',
+      name: 'karinushi',
+      // component: 
+    },
+    {
+      // 借主詳細頁面(含修改)
+      path: '/karinushi/info',
+      name: 'karinushiInfo',
+      // component:
     }
   ]
 })
